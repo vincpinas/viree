343ca6f25b350bf79079770be031723017cf79f4
Vincent Pinas <vincp@Vincents-MacBook-Pro.local>
2024-02-01 23:52:29 +0100

    demo update

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,12 +1,26 @@
 import './style.css'
-import { Renderer } from "../../src/gfx/renderer"
+import Renderer from "../../src/gfx/renderer"
+import Sprite from "../../src/gfx/sprite"
+import Vec2 from '../../src/math/vec2';
 
-let renderer = new Renderer({
-  wrapper: document.querySelector("#app")
-});     
-
-renderer.addUpdateFunction(() => {
-
+const sprite = new Sprite({
+  src: "./Rock-Sheet.webp",
+  pos: new Vec2(400, 150),
+  width: 100,
+  height: 100,
+  sheet_height: 100,
+  sheet_width: 2200,
+  interval: 60,
+  offset: 4
 })
+
+const renderer = new Renderer({
+  wrapper: document.querySelector("#app"),
+  autoClear: true,
+  autoResize: true
+})
+
+
+renderer.add(sprite)
 
 renderer.render()
